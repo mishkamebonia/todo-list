6e85192fd35d9f183d33d84ac170ea2fc504125a
mishkamebonia <60870999+mishkamebonia@users.noreply.github.com>
2023-04-15 01:14:11 +0400

    todolist
    test

---
+++ script.js
@@ -0,0 +1,32 @@
+const screen = document.getElementById("menu-input")
+const li = document.querySelectorAll(".item-container")
+const todoDiv = document.getElementById("todo-list")
+const textDiv = document.querySelectorAll(".item-container")
+let arr = []
+
+const addEvent = function() {
+  const newDiv = document.createElement("div")
+  newDiv.classList.add("item-container")
+  const newParagraph = document.createElement("p")
+  newParagraph.classList.add("list-item")
+  newDiv.appendChild(newParagraph)
+  newParagraph.innerHTML = screen.value
+  const arrLength = arr.push(todoDiv.appendChild(newDiv))
+  const numberParagraph = document.createElement("p")
+  numberParagraph.classList.add("number-order")
+  numberParagraph.innerHTML = arrLength
+  newDiv.appendChild(numberParagraph)
+
+  const removeBtn = document.createElement("button")
+  removeBtn.addEventListener("click", () => {
+    newDiv.innerHTML = ""
+  })
+
+  removeBtn.classList.add("remove-btn")
+  const icon = document.createElement("i")
+  icon.classList.add("fa-solid", "fa-xmark")
+  removeBtn.appendChild(icon)
+  newDiv.appendChild(removeBtn)
+
+  screen.value = ""
+}
